d4dade3344abf50604e52d1ca2fe06de5d2d258a
Filipe Veloso <filipe.roman.veloso@gmail.com>
2025-05-02 22:39:05 -0300

    remove osx

--- build-all.sh
+++ build-all.sh
@@ -20,14 +20,14 @@ cargo build --release --target x86_64-pc-windows-gnu
 cp target/x86_64-pc-windows-gnu/release/nocheat.dll target/release-builds/nocheat_windows_x86_64.dll
 
 # Build for macOS (x86_64)
-echo "Building for macOS (x86_64)..."
-cargo build --release --target x86_64-apple-darwin
-cp target/x86_64-apple-darwin/release/libnocheat.dylib target/release-builds/libnocheat_macos_x86_64.dylib
+# echo "Building for macOS (x86_64)..."
+# cargo build --release --target x86_64-apple-darwin
+# cp target/x86_64-apple-darwin/release/libnocheat.dylib target/release-builds/libnocheat_macos_x86_64.dylib
 
-# Build for macOS (ARM64/M1)
-echo "Building for macOS (ARM64/M1)..."
-cargo build --release --target aarch64-apple-darwin
-cp target/aarch64-apple-darwin/release/libnocheat.dylib target/release-builds/libnocheat_macos_arm64.dylib
+# # Build for macOS (ARM64/M1)
+# echo "Building for macOS (ARM64/M1)..."
+# cargo build --release --target aarch64-apple-darwin
+# cp target/aarch64-apple-darwin/release/libnocheat.dylib target/release-builds/libnocheat_macos_arm64.dylib
 
 echo "All builds completed successfully!"
 echo "Binaries are available in target/release-builds/"
